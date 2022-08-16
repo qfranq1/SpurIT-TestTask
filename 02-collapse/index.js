2583c4ac62a0a051df0ae00ab5d82306d93f8669
Uladzislau Leshka <vladik45812@gmail.com>
2022-08-16 19:03:21 +0300

    first commit

---
+++ 02-collapse/index.js
@@ -0,0 +1,33 @@
+let isContentVisible = true;
+
+const hiddenTitle = document.querySelector('.collapsible__action--hidden'),
+        visibleTitle = document.querySelector('.collapsible__action--visible'),
+        content = document.querySelector('.collapsible__content'),
+
+        contentHidden = [
+            { opacity: 1 },
+            { opacity: 0 }
+        ],
+        contentShown = [
+            { opacity: 0 },
+            { opacity: 1 }
+        ];
+
+document.querySelector('.collapsible__action--hidden').style.display = 'none';
+
+document.querySelector('.collapsible__button').onclick = () => {
+    if (isContentVisible) {
+        visibleTitle.style.display = 'none';
+        hiddenTitle.style.display = 'block';
+        content.animate(contentHidden, 100);
+        setTimeout(() => {
+            content.style.display = 'none';
+        }, 90);
+    }  else {
+        visibleTitle.style.display = 'block';
+        hiddenTitle.style.display = 'none';
+        content.style.display = 'block';
+        content.animate(contentShown, 100);
+    }
+    isContentVisible = !isContentVisible;
+}
